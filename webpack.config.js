3094766ed37ec11ffe7fd2a81ec24956dbe393ff
Ben <benawhiting@gmail.com>
2022-07-26 12:06:46 -0700

    Create separate .js file for home page

--- webpack.config.js
+++ webpack.config.js
@@ -2,7 +2,9 @@ const path = require('path');
 
 module.exports = {
     mode: 'none',
-    entry: './src/index.js',
+    entry: {
+        index: './src/index.js',
+    },
     output: {
         filename: 'bundle.js',
         path: path.resolve(__dirname, 'dist'),
